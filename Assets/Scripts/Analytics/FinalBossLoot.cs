a61352da4d9d25af6949a028403e9bfa54c44997
Tony G <ssj.tony89@gmail.com>
2020-03-26 18:16:29 +1100

    some analytics working

---
+++ Assets/Scripts/Analytics/FinalBossLoot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class FinalBossLoot : MonoBehaviour
+{
+	public string battleMvp = "Holtzmann";
+	public int mvpLevel = 34;
+	public float battleDurationInMinutes = 43.05f;
+
+	private void Start()
+	{
+		Analytics.CustomEvent("final_boss_defeated");
+		Analytics.CustomEvent("battle_mvp_selected", new Dictionary<string, object>()
+		{
+			{ "battle_mvp", battleMvp },
+			{ "mvp_level", mvpLevel },
+			{ "battle_time", battleDurationInMinutes }
+		});
+
+	}
+}
