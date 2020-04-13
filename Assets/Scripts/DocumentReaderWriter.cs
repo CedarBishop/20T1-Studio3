22c89f926e20d440e9a89cbee1ccbcf31530b5c7
Tony G <ssj.tony89@gmail.com>
2020-04-13 13:53:33 +1000

    added standard debugging file reader/writer tool

---
+++ Assets/Scripts/DocumentReaderWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class DocumentReaderWriter : MonoBehaviour
+{
+	private static string path = "Assets/Documents/SampleDocument.txt";
+
+	private void Start()
+	{
+		ReadString();
+		WriteString();
+	}
+
+	[MenuItem("Tools/Read file")]
+	static private void ReadString()
+	{
+		// Read the text directly from the .txt file
+		StreamReader reader = new StreamReader(path);
+		Debug.Log(reader.ReadToEnd());
+		reader.Close();
+	}
+
+	[MenuItem("Tools/Write file")]
+	static private void WriteString()
+	{
+		StreamWriter saveFile = new StreamWriter(path, false);
+		try
+		{
+			saveFile.WriteLine("Text that is wring over\n");
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+			throw;
+		}
+		finally
+		{
+			saveFile.Close();
+		}
+	}
+}
