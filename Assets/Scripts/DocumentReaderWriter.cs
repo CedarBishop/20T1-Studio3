e97d3b806f8c50f7ba4ec271d4be9d0ee1ec1bae
Tony G <ssj.tony89@gmail.com>
2020-04-13 16:29:44 +1000

    EULA agreement + EULA scene added
    also removed some trash files

--- Assets/Scripts/DocumentReaderWriter.cs
+++ Assets/Scripts/DocumentReaderWriter.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DocumentReaderWriter : MonoBehaviour
 {
-	public static string path = "Assets/Documents/OfficialEULA.txt";
+	public TextMeshProUGUI textObject;
 
-	[MenuItem("Tools/Read file")]
-	public static void ReadString()
+	public static string path = "Assets/Documents/OfficialEULA.txt";
+	private static StreamReader reader;
+
+	public Scene mainMenu;
+
+	private void Start()
 	{
+		textObject = textObject.GetComponent<TextMeshProUGUI>();
+
 		// Read the text directly from the .txt file
-		StreamReader reader = new StreamReader(path);
-		Debug.Log(reader.ReadToEnd());
+		reader = new StreamReader(path);
+		textObject.text = reader.ReadToEnd();
+
+		StartCoroutine(LoadMainMenu(8f));
+	}
+
+	IEnumerator LoadMainMenu(float waitTime)
+	{
+		yield return new WaitForSeconds(waitTime);
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public static void FinishedDisplayingEula()
+	{
 		reader.Close();
 	}
 }
